c7ab482ce3a683c5a56880d71f57306764591fb1
birsy <evan.c.muncy@gmail.com>
2022-12-30 17:00:17 -0500

    fix to darkness / blindness effects

--- src/main/java/birsy/foglooksgoodnow/client/ClientEvents.java
+++ src/main/java/birsy/foglooksgoodnow/client/ClientEvents.java
@@ -1,10 +1,8 @@
 package birsy.foglooksgoodnow.client;
 
 import birsy.foglooksgoodnow.FogLooksGoodNowMod;
-import birsy.foglooksgoodnow.config.FogLooksGoodNowConfig;
 import com.mojang.blaze3d.shaders.FogShape;
 import com.mojang.blaze3d.systems.RenderSystem;
-import net.minecraft.client.Minecraft;
 import net.minecraft.util.Mth;
 import net.minecraft.world.level.material.FogType;
 import net.minecraft.world.phys.Vec3;
@@ -12,7 +10,6 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ViewportEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.config.ModConfigEvent;
 
 @Mod.EventBusSubscriber(modid = FogLooksGoodNowMod.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class ClientEvents {
@@ -30,7 +27,7 @@ public class ClientEvents {
             }
 
             RenderSystem.setShaderFogStart(renderDistance * densityManager.fogStart.get((float) event.getPartialTick()));
-            RenderSystem.setShaderFogEnd(renderDistance * densityManager.fogDensity.get((float) event.getPartialTick()) * undergroundFogMultiplier);
+            RenderSystem.setShaderFogEnd(renderDistance * densityManager.fogEnd.get((float) event.getPartialTick()) * undergroundFogMultiplier);
             RenderSystem.setShaderFogShape(FogShape.SPHERE);
         }
     }
--- src/main/java/birsy/foglooksgoodnow/client/FogManager.java
+++ src/main/java/birsy/foglooksgoodnow/client/FogManager.java
@@ -3,25 +3,18 @@ package birsy.foglooksgoodnow.client;
 import birsy.foglooksgoodnow.FogLooksGoodNowMod;
 import birsy.foglooksgoodnow.config.FogLooksGoodNowConfig;
 import birsy.foglooksgoodnow.util.MathUtils;
-import com.google.common.collect.Lists;
-import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.DimensionSpecialEffects;
-import net.minecraft.client.renderer.FogRenderer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.CubicSampler;
 import net.minecraft.util.Mth;
-import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.LightLayer;
 import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.biome.BiomeManager;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.material.FogType;
 import net.minecraft.world.phys.Vec3;
@@ -42,7 +35,7 @@ public class FogManager {
 
     private final Minecraft mc;
     public InterpolatedValue fogStart;
-    public InterpolatedValue fogDensity;
+    public InterpolatedValue fogEnd;
     public InterpolatedValue currentSkyLight;
     public InterpolatedValue currentBlockLight;
     public InterpolatedValue currentLight;
@@ -58,7 +51,7 @@ public class FogManager {
     public FogManager() {
         this.mc = Minecraft.getInstance();
         this.fogStart = new InterpolatedValue(0.0F);
-        this.fogDensity = new InterpolatedValue(1.0F);
+        this.fogEnd = new InterpolatedValue(1.0F);
 
         this.currentSkyLight = new InterpolatedValue(16.0F);
         this.currentBlockLight = new InterpolatedValue(16.0F);
@@ -79,7 +72,7 @@ public class FogManager {
     public void initializeConfig() {
         FogLooksGoodNowMod.LOGGER.info("Initialized Config Values");
         this.fogStart.setDefaultValue(FogLooksGoodNowConfig.CLIENT_CONFIG.defaultFogStart.get());
-        this.fogDensity.setDefaultValue(FogLooksGoodNowConfig.CLIENT_CONFIG.defaultFogDensity.get());
+        this.fogEnd.setDefaultValue(FogLooksGoodNowConfig.CLIENT_CONFIG.defaultFogDensity.get());
         this.useCaveFog = FogLooksGoodNowConfig.CLIENT_CONFIG.useCaveFog.get();
         this.caveFogMultiplier = FogLooksGoodNowConfig.CLIENT_CONFIG.caveFogDensity.get();
         this.configMap = new HashMap<>();
@@ -115,7 +108,7 @@ public class FogManager {
             this.caveFogColors[1].interpolate(caveFogColor.y);
             this.caveFogColors[2].interpolate(caveFogColor.z);
         } else {
-            darknessAffectedFog = getDarknessEffectedFog(this.fogStart.defaultValue, this.fogDensity.defaultValue * density);
+            darknessAffectedFog = getDarknessEffectedFog(this.fogStart.defaultValue, this.fogEnd.defaultValue * density);
             this.caveFogColors[0].interpolate();
             this.caveFogColors[1].interpolate();
             this.caveFogColors[2].interpolate();
@@ -123,7 +116,7 @@ public class FogManager {
 
         this.darkness.interpolate(darknessAffectedFog[2]);
         this.fogStart.interpolate(darknessAffectedFog[0]);
-        this.fogDensity.interpolate(darknessAffectedFog[1]);
+        this.fogEnd.interpolate(darknessAffectedFog[1]);
 
         this.currentSkyLight.interpolate(mc.level.getBrightness(LightLayer.SKY, pos));
         this.currentBlockLight.interpolate(mc.level.getBrightness(LightLayer.BLOCK, pos));
@@ -151,35 +144,44 @@ public class FogManager {
         return Minecraft.getInstance().level.effects().skyType() == DimensionSpecialEffects.SkyType.NORMAL && densityManager.useCaveFog && Minecraft.getInstance().gameRenderer.getMainCamera().getFluidInCamera() == FogType.NONE;
     }
 
-    public static float[] getDarknessEffectedFog(float fs, float fd) {
+    public float[] getDarknessEffectedFog(float fs, float fd) {
         Minecraft mc = Minecraft.getInstance();
-        float renderDistance = mc.gameRenderer.getRenderDistance();
+        float renderDistance = mc.gameRenderer.getRenderDistance() * 16;
 
         Entity entity = mc.cameraEntity;
         float fogStart = fs;
-        float fogDensity = fd;
+        float fogEnd = fd;
         float darknessValue = 0.0F;
-
+        this.fogEnd.interpolationSpeed = 0.05f;
+        this.fogStart.interpolationSpeed = 0.05f;
         if (entity instanceof LivingEntity e) {
             if (e.hasEffect(MobEffects.BLINDNESS)) {
-                MobEffectInstance effect = e.getEffect(MobEffects.BLINDNESS);
-                float intensity = Mth.lerp(Math.min(1.0F, (float)effect.getDuration() / 20.0F), renderDistance, 5.0F);
-                fogStart = (5.0F * 0.5F) / renderDistance;
-                fogDensity = renderDistance / 10.0F;
+                fogStart = (4 * 16) / renderDistance;
+                fogEnd = (8 * 16) / renderDistance;
                 darknessValue = 1.0F;
-            }else if (e.hasEffect(MobEffects.DARKNESS)) {
+            } else if (e.hasEffect(MobEffects.DARKNESS)) {
                 MobEffectInstance effect = e.getEffect(MobEffects.DARKNESS);
                 if (!effect.getFactorData().isEmpty()) {
-                    float intensity = Mth.lerp(effect.getFactorData().get().getFactor(e, mc.getPartialTick()), renderDistance, 15.0F);
-                    fogStart = (15.0F * 0.2F) / renderDistance;
-                    fogDensity = renderDistance / 15.0F;
+                    float factor = this.mc.options.darknessEffectScale().get().floatValue();
+                    float intensity = effect.getFactorData().get().getFactor(e, mc.getPartialTick()) * factor;
+                    float darkness = 1 - (calculateDarknessScale(e, effect.getFactorData().get().getFactor(e, mc.getPartialTick()), mc.getPartialTick()));
+                    FogLooksGoodNowMod.LOGGER.info("" + intensity);
+                    fogStart = ((8.0F * 16) / renderDistance) * darkness;
+                    fogEnd = ((15.0F * 16) / renderDistance);
                     darknessValue = effect.getFactorData().get().getFactor(e, mc.getPartialTick());
                 }
             }
         }
 
-        return new float[]{fogStart, fogDensity, darknessValue};
+        return new float[]{fogStart, fogEnd, darknessValue};
     }
+
+    private float calculateDarknessScale(LivingEntity pEntity, float darknessFactor, float partialTicks) {
+        float factor = this.mc.options.darknessEffectScale().get().floatValue();
+        float f = 0.45F * darknessFactor;
+        return Math.max(0.0F, Mth.cos(((float)pEntity.tickCount - partialTicks) * (float)Math.PI * 0.025F) * f) * factor;
+    }
+
 
     public void close() {}
 
@@ -188,7 +190,7 @@ public class FogManager {
     public class InterpolatedValue {
         public float defaultValue;
 
-        private final float interpolationSpeed;
+        private float interpolationSpeed;
         private float previousValue;
         private float currentValue;
 
